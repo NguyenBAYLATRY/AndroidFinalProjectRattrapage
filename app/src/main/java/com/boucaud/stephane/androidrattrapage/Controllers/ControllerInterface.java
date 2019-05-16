f586d3f99560280d81d3235f36cef9c9ed77adb5
Stéphane Boucaud <stephane.boucaud@outlook.fr>
2019-05-16 20:22:39 +0100

    test

---
+++ app/src/main/java/com/boucaud/stephane/androidrattrapage/Controllers/ControllerInterface.java
@@ -0,0 +1,7 @@
+package com.boucaud.stephane.androidrattrapage.Controllers;
+
+import retrofit2.Callback;
+
+public interface ControllerInterface {
+    public void queryGetGenres(Callback actions);
+}
