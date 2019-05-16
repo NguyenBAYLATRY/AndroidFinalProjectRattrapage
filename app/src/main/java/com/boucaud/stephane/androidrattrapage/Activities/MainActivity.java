2eaf5803df0e8798c8a327a91976535af465681c
Stéphane Boucaud <stephane.boucaud@outlook.fr>
2019-05-16 18:58:14 +0100

    Creating Activities Package

--- app/src/main/java/com/boucaud/stephane/androidrattrapage/Activities/MainActivity.java
+++ app/src/main/java/com/boucaud/stephane/androidrattrapage/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.boucaud.stephane.androidrattrapage;
+package com.boucaud.stephane.androidrattrapage.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.boucaud.stephane.androidrattrapage.R;
 
 public class MainActivity extends AppCompatActivity {
 
