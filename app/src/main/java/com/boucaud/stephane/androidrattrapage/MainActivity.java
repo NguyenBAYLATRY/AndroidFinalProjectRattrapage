95004640ebfeb098e4193d6f1e9cacad9d0a294b
Stéphane Boucaud <stephane.boucaud@outlook.fr>
2019-05-16 18:55:20 +0100

    Project initiation
    - Description of the project
    - Informations
    - First app build

---
+++ app/src/main/java/com/boucaud/stephane/androidrattrapage/MainActivity.java
@@ -0,0 +1,13 @@
+package com.boucaud.stephane.androidrattrapage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
